b58c1682aa6a55f0a2b2cb6eab712c19c70e139f
David <david.stiven911@gmail.com>
2021-03-02 19:56:31 -0500

    testing material component form

--- view/src/app/app.component.ts
+++ view/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+import {FormControl, Validators} from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'shop-stalls';
+  title = 'Shop Stalls';
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Debes ingresar un correo';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  
 }

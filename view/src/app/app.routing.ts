4cfaa4d2684311701c9e64e417206e398ab7c384
David-sqrtpi <david.stiven911@gmail.com>
2021-03-25 15:59:30 -0500

    Router

--- view/src/app/app.routing.ts
+++ view/src/app/app.routing.ts
@@ -1,8 +1,12 @@
 import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
 import { LogInComponent } from "./components/log-in/log-in.component";
+import { SignUpComponent } from "./components/sign-up/sign-up.component";
 
 const appRoutes = [
-    {path:'login', component: LogInComponent, pathMatch:'full'}
+    {path:'login', component: LogInComponent, pathMatch:'full'},
+    {path:'', component: AppComponent, pathMatch:'full'},
+    {path:'sign-up', component: SignUpComponent, pathMatch:'full'}
 ]
 
 export const Routing = RouterModule.forRoot(appRoutes);

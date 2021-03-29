d8ed3bb763c4333cdaa78d7b43dae4f01c2be5ba
papeleria william <80803187+williamperez-11@users.noreply.github.com>
2021-03-28 22:45:47 -0500

    moodify

--- logic/src/main/java/application/api/ProductApi/ModifyProduct.java
+++ logic/src/main/java/application/api/ProductApi/ModifyProduct.java
@@ -4,7 +4,7 @@ import application.models.Product;
 import application.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin
 @RestController
 @RequestMapping ("product")
 public class ModifyProduct {

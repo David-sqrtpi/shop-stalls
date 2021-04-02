fe887edd7cdb13ecc4744ce7dd3a0d82de92a989
David-sqrtpi <david.stiven911@gmail.com>
2021-04-01 22:39:53 -0500

    w

--- logic/src/main/java/application/api/UserApi/GetUser.java
+++ logic/src/main/java/application/api/UserApi/GetUser.java
@@ -1,4 +1,25 @@
 package application.api.UserApi;
 
+import application.Repository.RepositoryUser;
+import application.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("user")
+@CrossOrigin
 public class GetUser {
+
+    @Autowired
+    private RepositoryUser repositoryUserService;
+
+    @GetMapping("{email}")
+    public User get(@PathVariable String email) {
+
+        return repositoryUserService.findByEmail(email);
+
+    }
+
 }

4e910733e5d6fefc4f174a271262f33e22df074f
papeleria william <80803187+williamperez-11@users.noreply.github.com>
2021-04-29 22:30:01 -0500

    Update http.service.ts

--- view/src/app/services/http.service.ts
+++ view/src/app/services/http.service.ts
@@ -1,17 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
-import { HttpClient } from '@angular/common/http'
-
+import { environment } from 'src/environments/environment';
+import { AuthHeadGeneratorService } from './auth-head-generator.service';
+const URI_API:string =  environment.url_backend + "services";
 @Injectable({
   providedIn: 'root'
 })
-export class HttpService {
+export class HttpServicesService {
 
-  private url:string = 'http://localhost:8080/';
+  private headers = this.header.generateHeader();
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private http:HttpClient, private header:AuthHeadGeneratorService) { }
 
-  add(data:object, endpoint:string){
-    return this.httpClient.post(this.url + endpoint, data)
+  addServices(service:object) {
+    return this.http.post(URI_API, service, {headers:this.headers, responseType:'text' as 'json'})
   }
+
+  getAllServices() {
+    return this.http.get(URI_API, {headers:this.headers});
+  }
+  
+  getServicesById(id:number) {
+    return this.http.get(URI_API+id, {headers:this.headers});
+  }
+
+  getServices(){
+    return this.http.get(URI_API, {headers:this.headers});
+  }
+
+  modifyProduct(body:object){
+    return this.http.put(URI_API+"1", body, {headers:this.headers});
+  }
+  
 }

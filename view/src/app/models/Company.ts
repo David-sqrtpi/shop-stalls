dc3bad5b7d8435b34f430e7b379f27124146840b
David-sqrtpi <david.stiven911@gmail.com>
2021-05-07 10:52:48 -0500

    model interfaces

---
+++ view/src/app/models/Company.ts
@@ -0,0 +1,7 @@
+export interface Company {
+    id: number,
+    name: string,
+    web: string,
+    address: string,
+    phone: string
+}
--- view/src/app/models/user.ts
+++ view/src/app/models/user.ts
@@ -1,4 +1,13 @@
+import { Company } from "./Company";
+import { Role } from "./Role";
+
 export interface User {
-    id:number, age:number, id_company:number;
-    name:string, email:string, password:string;
+    id: number,
+    name: string,
+    email: string,
+    password: string,
+    age: number,
+    state: string,
+    company:Company,
+    roles:Role []
 }

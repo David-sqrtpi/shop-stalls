e50a221a3a280b430ff4f2b5b1a5e58505e5537a
David-sqrtpi <david.stiven911@gmail.com>
2021-05-09 03:08:44 -0500

    get product from company

--- view/src/app/services/produt.service.ts
+++ view/src/app/services/produt.service.ts
@@ -4,14 +4,14 @@ import { environment } from 'src/environments/environment';
 import { Product } from '../models/Product';
 import { AuthHeadGeneratorService } from './auth-head-generator.service';
 
-const URI_API:string =  environment.url_backend + "products";
+const URI_API:string = environment.url_backend + "products";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpProdutService {
   private headers = this.header.generateHeader();
-
+  private company:number = +localStorage.getItem('company');
   constructor(private http:HttpClient, private header:AuthHeadGeneratorService) { }
 
   addProduct(product:object) {
@@ -19,11 +19,11 @@ export class HttpProdutService {
   }
 
   getProducts() {
-    return this.http.get(`${URI_API}/?company=${localStorage.getItem('company')}`, {headers:this.headers});
+    return this.http.get(`${URI_API}/?company=${this.company}`, {headers:this.headers});
   }
   
   getProductById(id:number) {
-    return this.http.get<Product>(`${URI_API}/${id}`, {headers:this.headers});
+    return this.http.get<Product>(`${URI_API}/${id}?company=${this.company}`, {headers:this.headers});
   }
 
   modifyProduct(body:object){

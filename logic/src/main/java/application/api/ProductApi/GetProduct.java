720f5dc858dfd30ead0f51d5c143919aa64cf477
David-sqrtpi <david.stiven911@gmail.com>
2021-05-09 00:22:34 -0500

    mer-app

--- logic/src/main/java/application/api/ProductApi/GetProduct.java
+++ logic/src/main/java/application/api/ProductApi/GetProduct.java
@@ -5,8 +5,7 @@ import application.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@CrossOrigin
+
 public class GetProduct {
     @Autowired
     private ProductRepository repositoryProductService;
---
+++ logic/src/main/java/application/api/inventory/GetInventory.java
@@ -0,0 +1,23 @@
+package application.api.inventory;
+
+import application.Repository.InventoryRepository;
+import application.entity.Inventory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class GetInventory {
+    @Autowired
+    private InventoryRepository inventoryRepository;
+
+    @GetMapping("companies/{company}/inventory")
+    public List<Inventory> get(@PathVariable long company) {
+        return inventoryRepository.findByProductCompanyId(company);
+    }
+}
